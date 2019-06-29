23ed6acfc2654a7955c7db6ea698dd1b25dd0a4d
Frustrated Learner <frustratedlearner007@gmail.com>
2019-06-29 13:47:58 +0530

    second version of app code

---
+++ app.py
@@ -0,0 +1,23 @@
+print("Frustrated Learner")
+print('0----')
+print(' ||||')
+print('*' * 10)
+price = 10
+price = 20
+print(price)
+full_name = "John Smith"
+age = 20
+is_new = True
+name = input('What is your name? ')
+print('Hi '+ name)
+name = input('What is your name? ')
+color = input('What is your favorite colour? ')
+print(name + ' likes ' + color)
+birth_year = input('What is your birth year? ')
+age = 2019 -int(birth_year)
+print(age)
+print(type(birth_year))
+print(type(age))
+weight_pounds = input('What is your weight?' )
+weight_kg = int(weight_pounds) * 0.453592
+print("Weight in KGs is " + str(weight_kg))
